e64a4fd53ee5e452663d3d84cae266756f3c21a6
KLAY <khaled934533@gmail.com>
2026-09-26 00:42:30 +0300

    types.rs

--- src/protocol/types.rs
+++ src/protocol/types.rs
@@ -4,10 +4,12 @@ use serde::{Deserialize, Serialize};
 pub struct Player {
     pub id: String,
     pub name: String,
+    pub ready: bool,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Session {
     pub id: String,
     pub players: Vec<Player>,
+    pub started: bool,
 }
