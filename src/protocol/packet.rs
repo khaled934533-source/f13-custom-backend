0fcadfcdef12dffab8af6340557ecdfaa878c5f5
KLAY <khaled934533@gmail.com>
2026-09-25 23:17:39 +0300

    packet.rs

--- src/protocol/packet.rs
+++ src/protocol/packet.rs
@@ -1,8 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+pub const MAGIC: [u8; 4] = *b"KLAY";
+pub const VERSION: u8 = 1;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Packet<T> {
-    pub version: u16,
+    pub version: u8,
+    pub message_id: u16,
     pub request_id: u32,
     pub payload: T,
 }
